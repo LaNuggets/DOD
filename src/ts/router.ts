f5f5964eeb0afce86d6030000c34b7965ead2556
Godwin <godoblasse@gmail.com>
2026-02-16 10:28:03 +0100

    Creation of a new channel

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -2,6 +2,7 @@ import HomeView from "@/views/HomeView.vue";
 import ConnexionView from "@/views/ConnexionView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import { createRouter, createWebHistory} from "vue-router";
+import CreateChannelView from "@/views/CreateChannelView.vue";
 
 export default createRouter({
     history : createWebHistory(),
@@ -16,6 +17,11 @@ export default createRouter({
             name : "Connexion",
             component : ConnexionView,
             
+        },
+        {
+            path: '/protected/channel',
+            name : "Channel",
+            component : CreateChannelView
         },
         {
             path: '/:pathMatch(.*)*',
---
+++ src/views/CreateChannelView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CreateChannel from '@/components/CreateChannel.vue';
+</script>
+
+<template>
+	<CreateChannel></CreateChannel>
+</template>
+
+<style scoped></style>

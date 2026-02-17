2f3f90e89b4a8a17ab894021e95ecdfc43d54e94
kelyan danis <kelyan.danis@ynov.com>
2026-02-17 09:08:36 +0100

    ✨ Feat : added the sengin of textual messages ✨

--- src/components/MessageRainbow.vue
+++ src/components/MessageRainbow.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { saveToken } from '@/ts/saveload.ts';
-import router from "@/ts/router";
+import { useRoute } from "vue-router";
 import { ref } from 'vue';
+import { useStore } from '@/ts/store'
 
 interface Message {
-    type: string, // Image or 
+    type: string, // Image or Text
     value: string,
 };
-
 
 const message : Message = {
   type : '',
   value : '',
 }
+
+const tokenStore = useStore()
+
+const route = useRoute();
+const id = route.params.id as string;
 
 const loading = ref(false)
 const error = ref<string | null>(null)
@@ -20,22 +24,23 @@ const error = ref<string | null>(null)
 const postMessage = async () => {
 
     // Define the image type
-    if (message.value) {
-
-    } else {
-        message.type = "";
-    };
-
+    message.type = "Text";
+    if (id == undefined) {
+        error.value = "You'r curently outside a chanel you c'ant post message !"
+        return
+    }
     loading.value = true
     error.value = null
 
     try {
         const requestBody: Message = message
+        const token = tokenStore.getToken()
     
-        const response = await fetch("https://edu.tardigrade.land/msg/protected/channel/"+ 1 +"/message", {
+        const response = await fetch("https://edu.tardigrade.land/msg/protected/channel/"+ id +"/message", {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify(requestBody)
     })
@@ -45,7 +50,7 @@ const postMessage = async () => {
         throw new Error(errorData.message || `Erreur ${response.status}`)
     }
 
-    const data: Response = await response.json()
+    const data = await response.json()
 
     } catch (e: unknown) {
         error.value = e instanceof Error ? e.message : 'Error during conection'
@@ -56,6 +61,7 @@ const postMessage = async () => {
 </script>
 
 <template>
+    
     <form @submit.prevent="postMessage" class="message-form">
         <div>
             <input 

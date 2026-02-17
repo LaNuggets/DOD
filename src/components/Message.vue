e4c141787e78ec7a2f0d68f7ded45c1fbfbf5949
kelyan danis <kelyan.danis@ynov.com>
2026-02-17 13:39:19 +0100

    ✨ Feat : add the websocket ✨

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,18 +1,71 @@
 <script setup lang="ts">
 import MessageRainbow from '@/components/MessageRainbow.vue';
 import { useRoute } from 'vue-router';
-import { watch, onMounted} from 'vue';
+import { watch, onMounted, ref, onUpdated, onUnmounted} from 'vue';
+import MessageBox from './MessageBox.vue';
+import { useStore } from '@/ts/store';
 
+interface Content {
+    type: string, // Image or Text
+    value: string,
+}
+
+interface Message {
+    channel_id: number,
+    timestamp: Date,
+    author: string,
+    content: Content
+};
+
+let ws: WebSocket | null = null
+const tokenStore = useStore()
 const route = useRoute();
+const loading = ref(false)
+const error = ref<string | null>(null)
+const messages = ref<Message[]>([])
 
+async function loadMessage(id : string) {
+    try {
+        const token = tokenStore.getToken()
+    
+        const response = await fetch('https://edu.tardigrade.land/msg/protected/channel/'+ id +'/messages/0', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
 
-function loadMessage(id : string) {
-	console.log("Channel ID:", id);
-	// for you my kyky
+    if (!response.ok) {
+        const errorData = await response.json()
+        throw new Error(errorData.message || `Erreur ${response.status}`)
+    }
+
+    messages.value = await response.json()
+
+    } catch (e: unknown) {
+        error.value = e instanceof Error ? e.message : 'Error during conection'
+    } finally {
+        loading.value = false
+    }
+};
+
+const connect = (id : string) => {
+    const token = tokenStore.getToken()
+    ws = new WebSocket('https://edu.tardigrade.land/msg/ws/channel/'+id+'/token/'+token)
+
+    ws.onopen = () => console.log('✅ Connecté')
+    ws.onmessage = (event) => {
+        console.log(event.data)
+        messages.value.push(JSON.parse(event.data) as Message)
+    }
+    ws.onclose = () => console.log('❌ Déconnecté')
+    ws.onerror = (error) => console.error('Erreur:', error)
 }
 
 onMounted(() => {
 	loadMessage(route.params.id as string);
+    connect(route.params.id as string);
 });
 
 
@@ -21,15 +74,23 @@ watch(
 	() => route.params.id as string,
 	(newId) => {
 		loadMessage(newId)
+        connect(newId)
 	}
 )
 
+onUnmounted(() => ws?.close())
 
 </script>
 
 <template>
 
-<h1>Hi</h1>
+<div>
+    <p>{{ loading ? 'Loading messages history ...' : 'Messages' }}</p>
+    <p v-if="error" class="error">{{ error }}</p>
+    <div v-else v-for="message in messages" > 
+        <MessageBox :data="message"/>
+    </div>
+</div>
 <MessageRainbow />
 
 </template>
--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -1,6 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+defineProps<{
+  data: Message
+}>()
+
+interface Content {
+    type: string, // Image or Text
+    value: string,
+}
+
+interface Message {
+    channel_id: number,
+    timestamp: Date,
+    author: string,
+    content: Content
+};
+
+</script>
 
 <template>
+    <div>
+        <p>{{ data.author }}</p>
+        <p v-if="new Date(data.timestamp).getDate() != new Date(Date.now()).getDate()">{{ new Date(data.timestamp).toLocaleString() }}</p>
+        <p v-else>{{ new Date(data.timestamp).getHours() + ":" + new Date(data.timestamp).getMinutes()}}</p>
+        <p v-if="data.content.type == 'Text'">{{ data.content.value }}</p>
+        <img v-else :src="data.content.value" />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
